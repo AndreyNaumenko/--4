383df6d2a1e9cb6ebc199f2ab16e8cad3ac8b619
Andrey <rolland81280@gmail.com>
2022-09-16 00:21:59 +0300

    сохран3

--- Problem-25/Program.cs
+++ Problem-25/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+string nameA = Console.ReadLine();
+int numberA = Convert.ToInt32(nameA);
+Console.WriteLine("Введите второе число ");
+string nameB = Console.ReadLine();
+int numberB = Convert.ToInt32(nameB);
+if (numberB > 0)
+{
+    double a = Math.Pow(numberA,numberB);
+    Console.WriteLine("Натуральная степень первого числа " + a);
+}
+else
+{ 
+   Console.WriteLine("Обращаем ваше внимание,  разбирается понятие степени только с натуральным показателем");
+}
