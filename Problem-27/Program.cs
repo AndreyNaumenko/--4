45e394d2ceb535757fcb987834ba9cb5100d36e5
Andrey <rolland81280@gmail.com>
2022-09-16 00:46:55 +0300

    сохран5

---
+++ Problem-27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число ");
+string name = Console.ReadLine();
+int number = Convert.ToInt32(name);
+int sum = 0;
+if (number < 0)
+{
+    number = number * -1;
+}
+while (number > 0)
+{
+    int numberA = number % 10;
+    sum = sum + numberA;
+    number = number / 10;
+}
+Console.WriteLine("Сумма цифр в числе " + sum);
