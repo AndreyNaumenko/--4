4eb8c673b819d9457ee291b9c00a864f5f0575bb
Andrey <rolland81280@gmail.com>
2022-09-16 02:00:45 +0300

    сохран6

---
+++ Problem-29/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,1000);
+}
+int count = array.Length;
+int position = 0;
+while (position < count)
+{
+    System.Console.Write(array[position] + " ");
+    position++;
+}
